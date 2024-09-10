7152f3894d51d17fb8a78f07041588253451e57a
Liyema <Liyema@DESKTOP-JKPG5BO>
2024-09-10 16:09:02 +0200

    Add project files.

---
+++ PROG1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using PROG1User.Models;
+using System.Diagnostics;
+
+namespace PROG1User.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ClaimManager()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Review()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
